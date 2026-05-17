ffca978a635da8590fdcfa6d489996b71b243c29
Vamshi Krishna Emmadi <vamshikris25@gmail.com>
2026-05-17 21:59:33 +0530

    New changes

--- python-for-ai/Control Flow/Loops/for-loops.py
+++ python-for-ai/Control Flow/Loops/for-loops.py
@@ -1,6 +1,33 @@
+     #Repeat a specific number of times
 #Print numbers from 0 to 4
 #It counts from zero to Four
+#Range - 5 means it prints 0-4 (known as indexes)
 for i in range (5):
     print(i)
 
-#Range - 5 means it prints 0-4 (known as indexes)
+
+
+
+#Count from different starting points
+#Count from 2 to 7
+for i in range (2,8):
+    print(i)
+
+#Count by 2s:-
+for i in range (0,10,2):
+    print(i)
+
+#Count by 5s:-
+for i in range (0,45,5):
+    print(i)
+
+#Loop through text
+
+name = "Vamshi"
+for letter in name:
+    print(letter)
+
+#Loop through a list (preview - basics)
+colors = ["red", "blue", "green", "yellow"]
+for color in colors:
+    print(f"I like {color}")
