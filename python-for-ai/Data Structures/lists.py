12a45a55f67fee332c37cc5d1a3f51ea741158ec
Vamshi Krishna Emmadi <vamshikris25@gmail.com>
2026-05-20 16:24:15 +0530

    Create lists.py

---
+++ python-for-ai/Data Structures/lists.py
@@ -0,0 +1,20 @@
+#Empty list
+
+my_list = []
+
+#List with items
+fruits = ["apple", "banana", "orange"]
+numbers = [1,2,3,4,5]
+mixed = ["hello", 42, True, 3.14]
+
+
+
+has_license = True
+
+my_list = ["Alice", 25, age, True, has_license]
+
+my_list[0] = "Vamshi"
+my_list[1] = 21
+
+my_list.append("Krishna")
+my_list.remove(age)
